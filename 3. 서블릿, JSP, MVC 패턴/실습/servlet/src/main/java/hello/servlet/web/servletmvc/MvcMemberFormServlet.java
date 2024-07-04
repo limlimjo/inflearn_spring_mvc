22d88f78b97f2251906215efb31523b38f81507a
selim-jo <skyblue.777@daum.net>
2024-07-04 18:49:27 +0900

    [add] 서블릿, JSP, MVC 패턴(MVC 패턴-적용(실습))

---
+++ 3. 서블릿, JSP, MVC 패턴/실습/servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -0,0 +1,23 @@
+package hello.servlet.web.servletmvc;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFormServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // /WEB-INF 경로 안에 JSP가 있으면 외부에서 직접 JSP를 호출할 수 없음
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        // dispatcher.forward: 다른 서블릿이나 JSP로 이동할 수 있는 기능임 (서버 내부에서 다시 호출이 발생함)
+        dispatcher.forward(request, response);
+    }
+}
