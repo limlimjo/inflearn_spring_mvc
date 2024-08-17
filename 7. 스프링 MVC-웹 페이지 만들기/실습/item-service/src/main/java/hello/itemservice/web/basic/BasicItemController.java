fbcf8de451de7ba9ef24fce0bb14087071a3d412
selim-jo <skyblue.777@daum.net>
2024-08-17 13:33:36 +0900

    [add] 스프링 MVC-웹 페이지 만들기(상품 등록 처리-@RequestParam, @ModelAttribute(실습))

--- 7. 스프링 MVC-웹 페이지 만들기/실습/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ 7. 스프링 MVC-웹 페이지 만들기/실습/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,9 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,6 +36,52 @@ public class BasicItemController {
     @GetMapping("/add")
     public String addForm() {
         return "basic/addForm";
+    }
+
+    // 상품 등록 처리 - @RequestParam 이용
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam("itemName") String itemName,
+                            @RequestParam("price") int price,
+                            @RequestParam("quantity") Integer quantity,
+                            Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    // 상품 등록 처리 - @ModelAttribute 이용
+    /**
+     * @ModelAttribute name 생략 가능
+     * model.addAttribute(item); 자동 추가, 생략 가능
+     * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
+     */
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item,
+                            Model model) {
+        itemRepository.save(item);
+        //model.addAttribute("item", item); //자동 추가, 생략 가능
+        return "basic/item";
+    }
+
+    // 상품 등록 처리 - @ModelAttribute 이용 (@ModelAttribute 이름 생략)
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item,
+                            Model model) {
+        itemRepository.save(item);
+        //model.addAttribute("item", item); //자동 추가, 생략 가능
+        return "basic/item";
+    }
+
+    // 상품 등록 처리 - @ModelAttribute 이용 (@ModelAttribute 전체 생략)
+    @PostMapping("/add")
+    public String addItemV4(Item item, Model model) {
+        itemRepository.save(item);
+        //model.addAttribute("item", item); //자동 추가, 생략 가능
+        return "basic/item";
     }
 
     // @PostConstruct: 해당 빈의 의존관계가 모두 주입되고 나면 초기화 용도로 호출됨
